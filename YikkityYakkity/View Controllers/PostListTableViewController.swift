5607dbd4bd20f763f2581c79db3d65b0126189a0
Julia Rodriguez <juliarodriguez@Julias-MacBook-Pro.local>
2019-07-11 19:23:58 -0600

    completed table view controller

--- YikkityYakkity/View Controllers/PostListTableViewController.swift
+++ YikkityYakkity/View Controllers/PostListTableViewController.swift
@@ -12,45 +12,86 @@ class PostListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        fetchPost()
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as? PostTableViewCell
 
-        // Configure the cell...
-
-        return cell
+        let post = PostController.shared.posts[indexPath.row]
+        cell?.post = post
+        
+        // nil coalesce a uitableview cell
+        return cell ?? UITableViewCell()
     }
-    */
+
 
     //MARK: = Bar Button Items
     @IBAction func createNewPostButtonTapped(_ sender: Any) {
-        
+        presentSimpleAlert(title: "Add a new Post!", message: "")
     }
     
     @IBAction func refreshButtonTapped(_ sender: Any) {
         
     }
     
+    func presentSimpleAlert(title: String, message: String) {
+        // Instatiating our Alert Controller, passing in our parameters for title and message
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // add textfields -OR- define with a name var messageTextField 
+        alertController.addTextField { (textfield) in
+            textfield.placeholder = "Add Message here..."
+            
+        }
+        
+        alertController.addTextField { (textfield) in
+            textfield.placeholder = "Your alias here"
+        }
+        
+        // Add a post Action that grabs our text from our textFields, and passes those values into our createPost function
+        let postAction = UIAlertAction(title: "Post", style: .default) { (_) in
+            guard let bodyText = alertController.textFields?[0].text,
+            let authorText = alertController.textFields?[1].text,
+            !bodyText.isEmpty,
+            !authorText.isEmpty
+                else { return }
+         
+            PostController.shared.createPost(text: bodyText, author: authorText, completion: { (_) in
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            })
+        }
+        
+        // Add our cancel action
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        // Add the action to our alertController
+        alertController.addAction(postAction)
+        alertController.addAction(cancelAction)
+        // Present the alertController View
+        self.present(alertController, animated: true )
+        
+        }
+        
+    }
+    
+    
+    func fetchPost() {
+    PostController.shared.fetchAllPosts { (_) in
+    DispatchQueue.main.async {
+    self.tableView.reloadData()
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -62,4 +103,3 @@ class PostListTableViewController: UITableViewController {
     }
     */
 
-}
--- YikkityYakkity/Views/PostTableViewCell.swift
+++ YikkityYakkity/Views/PostTableViewCell.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class PostTableViewCell: UITableViewCell {
     
+    // landing pad 
+    var post: Post? {
+     didSet {
+        updateViews()
+        }
+    }
+    
     @IBOutlet weak var postTextLabel: UILabel!
     @IBOutlet weak var pointTotalLabel: UILabel!
     
     
     @IBAction func upvoteButtonTapped(_ sender: Any) {
+        guard let post = post else { return }
+        post.score += 1
+        pointTotalLabel.text = "\(post.score)"
         
     }
     @IBAction func downvoteButtonTapped(_ sender: Any) {
+        guard let post = post else { return }
+        post.score -= 1
+        pointTotalLabel.text = "\(post.score)"
         
     }
+    func updateViews() {
+        guard let post = post else { return }
+        postTextLabel.text = "\(post.text) \n - \(post.author)"
+        pointTotalLabel.text = "\(post.score)"
+    }
 
-}
